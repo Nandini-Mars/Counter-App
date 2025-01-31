2f1c017d61c5cf7c3b01fba91bd82df1838e1cd0
Nandini-Mars <nandini.mailbox@gmail.com>
2025-01-31 16:00:24 +0530

    counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -15,7 +15,7 @@ const Counter = () => {
     return (
         <>
         <h1>Counter App</h1>
-        <p>Count:{count}</p>
+        <p>Count: {count}</p>
         <button type="button" onClick={handleIncrement}>Increment</button>
         <button type="button" onClick={handleDecrement}>Decrement</button>
         </>
